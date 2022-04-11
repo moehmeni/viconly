b4833d644a06ba213e6bc59082fa085fd4611854
mdmomeni <moehmeni@gmail.com>
2022-04-11 16:57:25 +0430

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Viconly from '@/viconly.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Viconly
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <viconly />
+  </div>
+</template>
---
+++ src/viconly.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div
+    :style="{ fontSize: size + 'rem', color }"
+    class="viconly-icon-container"
+  >
+    <i
+      :class="{
+        icbo: bold,
+        ['iconly-' + icon]: true,
+      }"
+    ></i>
+  </div>
+</template>
+
+<script>
+import "@/iconly/style.css";
+export default /*#__PURE__*/ {
+  name: "Viconly", // vue component name
+  props: {
+    icon: {
+      type: String,
+      required: true,
+      default: "Home",
+    },
+    color: {
+      type: String,
+      default: "#ffffff",
+    },
+    bold: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: "0.6",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.viconly-icon-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
