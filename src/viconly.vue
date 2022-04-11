7f9281e978f189242c0fc5ccc13259222cfbad85
mdmomeni <moehmeni@gmail.com>
2022-04-11 20:37:26 +0430

    Add bulk and light style

--- src/viconly.vue
+++ src/viconly.vue
@@ -5,29 +5,44 @@
     class="viconly-icon-container"
   >
     <i
+      v-if="!bulk"
       :class="{
         icbo: bold,
+        icli: light,
         ['iconly-' + icon]: true,
       }"
-    ></i>
+    >
+    </i>
+    <span v-else :class="{ ['iconlyBulk-' + icon]: true }">
+      <span class="path1"></span>
+      <span class="path2"></span>
+    </span>
   </div>
 </template>
 
 <script>
 import "@/iconly/style.css";
+import "@/iconly/bulk-style.css";
 export default /*#__PURE__*/ {
   name: "Viconly", // vue component name
   props: {
     icon: {
       type: String,
       required: true,
-      default: "Home",
     },
     color: {
       type: String,
       default: "#ffffff",
     },
     bold: {
+      type: Boolean,
+      default: false,
+    },
+    light: {
+      type: Boolean,
+      default: false,
+    },
+    bulk: {
       type: Boolean,
       default: false,
     },
